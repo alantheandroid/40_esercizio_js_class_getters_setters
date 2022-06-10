d066c57a0b7cbc7c346564011b272cfc2715b14d
alantheandroid <alessio.ilardi@gmail.com>
2022-06-10 09:09:57 +0200

    feat: add setters and getter

--- exercise-7.js
+++ exercise-7.js
@@ -1,10 +1,27 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age) {
+    this.name = firstName;
+    this.surname = lastName;
+    this.years = age;
+  }
+  set firstName(value) {
+    this.name = value;
+  }
+  set lastName(value) {
+    this.surname = value;
+  }
+  set age(number) {
+    this.years = number;
+  }
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  }
 }
 
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
+/* console.log(person); */
